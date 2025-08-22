92bcb52a5950e1b6995dd919f7e59454b216cfbe
maximt23-chat <maxtrubitsyn2@mail.ru>
2025-08-22 11:24:28 +0300

    Типы тестов
    *Рассмотрел разницу между getBy/getAll, findBy/findAll, queryBy/queryAll
    *Познакомился с fireEvent и его методами
    *Познакомился с userEvent и его методами
    *Рассмотрел data-userid метатег
    *Установил userEvent выше 13.0 (ниже было без асинхронных запросов, выше теперь используется async await)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [text, setText] = useState('');
+  const [userText, setUserText] = useState('');
+  const onClickToggle = () => {
+    setToggle(prev => !prev);
+  }
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 100)
+  })
   return (
     <div>
+      <h1 data-testid='h1Test'>{text}</h1>
+      <h2 data-testid='userH1'>{userText}</h2>
+      {toggle === true && <div data-testid="toggle-text">toggle text</div>}
+      {data && <div style={{color: "red"}}>text</div>}
       <h1>Hello world</h1>
-      <button>click me</button>
+      <button onClick={onClickToggle} data-testid="toggle-button">click me</button>
+      <input type="text" placeholder='user event input' onChange={e => setUserText(e.target.value)}/>
+      <input type="text" placeholder='new input' onChange={e => setText(e.target.value)}/>
       <input type="text" placeholder='some input...'/>
     </div>
   )
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,6 @@
 import { describe, test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
 describe('renders learn react link', () => {
@@ -26,5 +27,53 @@ describe('renders learn react link', () => {
             А дальше мы можем либо откатиться и исправить input, либо указать, что изменения валидные и перезаписать snapshot
         */
         expect(input).toMatchSnapshot();
+    })
+    test('renders get by find', async () => {
+        render(<App/>)
+        screen.debug(); //Перем тем, как мы получили элемент
+        const elem = await screen.findByText(/text/i);
+        expect(elem).toBeInTheDocument();
+        screen.debug(); //После изменения в dom дереве
+    })
+    test('react btn', () => {
+        render(<App/>);
+        const btn = screen.getByTestId('toggle-button');
+        const elem = screen.queryByTestId('toggle-text');
+        expect(screen.queryByTestId('toggle-text')).toBeNull(); //пока что блока div не существует, т.к. кнопка не была нажата
+        fireEvent.click(btn); //fireEvent позволяет получать какие-то события из блоков кода. Например click, change и т.д
+        expect(screen.queryByTestId('toggle-text')).toBeInTheDocument(); //После первого нажатия toggle стал true и div отрисовался
+        fireEvent.click(btn, /*Можем передать какие-то доп опции*/); //Второе нажатие
+        expect(screen.queryByTestId('toggle-text')).toBeNull(); //Теперь toggle стал false и div размонтировался со страницы
+
+        /*
+            Очень важное замечание, что если мы положим ссылку на проверяемый объект в переменную (в моем примере это elem) и будем
+            работать далее с этой переменной, то мы как бы в переменной зафиксируем первоначальное состояние ссылки на объект, условно,
+            объект был null, а потом заполнился чем-то, но, так как он в ссылке, у него и останется null, т.к. тесты будут проверять
+            все сразу и не будут дожидаться изменений. В таком случае, мы можем либо сделать тест ассинхронным и дожидаться 
+            изменений, либо просто вложить путь к объекту внутрь expect, как сделано выше. Таким образом мы всегда будем получать
+            актуальное состояние проверяемого объекта
+        */
+    })
+    test('Новый инпут', () => {
+        render(<App/>);
+        const input = screen.getByPlaceholderText('new input');
+        expect(screen.queryByTestId('h1Test')).toContainHTML(''); //Проверяем, что содержимое h1 === ''
+        fireEvent.input(input, {
+            target: { value: '123' } //Передаем value так, как будто бы мы что-то написали в input через e.target.value
+        })
+        expect(screen.queryByTestId('h1Test')).toContainHTML('123') //Говорим о том, что h1 теперь содержит текст 123
+        /*
+            fireEvent позволяет эмулировать только событие, то есть, нажатие, изменение и прочее. Однако, если же мы хотим 
+            эмулировать полное поведение пользователя, ам нужно использовать userEvent, там мы прям сможем любое поведение,
+            нажатие на любую клавишу, движение мыши, использовать
+        */
+    })
+    test('user event', async () => {
+        const user = userEvent.setup();
+        render(<App/>);
+        const input = screen.getByPlaceholderText('user event input');
+        expect(screen.queryByTestId('userH1')).toContainHTML('');
+        await user.type(input, '321');
+        expect(screen.queryByTestId('userH1')).toContainHTML('321');
     })
 })
